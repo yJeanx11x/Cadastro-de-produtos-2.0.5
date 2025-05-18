5eb2ee02cea547160b266b5200ad3588d02ba8d7
Jean Lucas <j3anlucas010@gmail.com>
2025-05-18 08:49:48 -0300

    :feat: adicionar captura de dados do formulário e prevenir envio padrão

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const nomeProduto=document.querySelector('#nome')
+const precoProduto=document.querySelector('#preco')
+const btnCadastro=document.querySelector('#btn-cadastro')
+
+btnCadastro.addEventListener('click',(e)=>{
+    e.preventDefault()
+
+})
