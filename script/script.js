c70052fb0767c54979adcdcb97703147e81cdde0
Jean Lucas <j3anlucas010@gmail.com>
2025-05-18 08:56:32 -0300

    :sparkles:"captura de dados do formulário e prevenção do envio padrão"

--- script/script.js
+++ script/script.js
@@ -2,7 +2,9 @@ const nomeProduto=document.querySelector('#nome')
 const precoProduto=document.querySelector('#preco')
 const btnCadastro=document.querySelector('#btn-cadastro')
 
+
 btnCadastro.addEventListener('click',(e)=>{
+//Não envia o formulario
     e.preventDefault()
 
 })
