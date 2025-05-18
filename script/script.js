3ae311d4e2be89b76fdbd7838e4d778efa1912e1
Jean Lucas <j3anlucas010@gmail.com>
2025-05-18 15:36:04 -0300

    :sparkles: feat: "validação de campos e cadastro de produto com alerta de sucesso"

--- script/script.js
+++ script/script.js
@@ -1,10 +1,25 @@
 const nomeProduto=document.querySelector('#nome')
 const precoProduto=document.querySelector('#preco')
+const quantidadeP=document.querySelector('#quantidade')
 const btnCadastro=document.querySelector('#btn-cadastro')
 
 
 btnCadastro.addEventListener('click',(e)=>{
 //Não envia o formulario
     e.preventDefault()
+
+    if(nomeProduto.value == '' && precoProduto.value =='' && quantidadeP.value == ''){
+        console.log('erro')
+    }else{
+        const estoque=[
+            {nome:`${nomeProduto.value}`,preco:`${precoProduto.value}`,qtd:`${quantidadeP.value}`}
+        ]
+        alert(`Cadastro realizado com sucesso! Seu produto foi adicionado ao sistema.`)
+
+        console.log(estoque)
+    }
+    nomeProduto.value=''
+    precoProduto.value=''
+    quantidadeP.value=''
 
 })
