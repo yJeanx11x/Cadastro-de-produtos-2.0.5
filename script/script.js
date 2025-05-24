f4fa4b41d43825a51376a2ba82a033180f4a7c15
Jean Lucas <j3anlucas010@gmail.com>
2025-05-24 11:06:21 -0300

    "feat: :sparkles: "add funcionalidade de verificação de Produtos Cadastrados"

--- script/script.js
+++ script/script.js
@@ -1,25 +1,41 @@
-const nomeProduto=document.querySelector('#nome')
-const precoProduto=document.querySelector('#preco')
-const quantidadeP=document.querySelector('#quantidade')
-const btnCadastro=document.querySelector('#btn-cadastro')
+const nomeProduto = document.querySelector("#nome");
+const precoProduto = document.querySelector("#preco");
+const quantidadeP = document.querySelector("#quantidade");
+const btnCadastro = document.querySelector("#btn-cadastro");
+
+btnCadastro.addEventListener("click", (e) => {
+  //Não envia o formulario
+  e.preventDefault();
+  
+  // Armazena o Nomes dos Produtos
+  const nomesProduto = ['xbox s s'];
+  
+  // verifica se os campos estão prenchidos se não erro 
+  if (
+      nomeProduto.value == "" && precoProduto.value == "" && quantidadeP.value == "") {
+          console.log("erro");
+        }
+        // Verifica se á um produto com esse nome 
+        if(nomesProduto.includes(`${nomeProduto.value}`)== true){
+            alert('⚠️ Este produto já está cadastrado no sistema.')
+            
+        }else {
+            nomesProduto.push(`${nomeProduto.value}`);
+            const produto = {
+      id:`${nomesProduto.length}`,
+      nome: `${nomeProduto.value}`,
+      preco: `${precoProduto.value}`,
+      qtd: `${quantidadeP.value}`,
+    };
+    return produto 
+  }
+  
+  nomeProduto.value = "";
+  precoProduto.value = "";
+  quantidadeP.value = "";
+  
+});
 
 
-btnCadastro.addEventListener('click',(e)=>{
-//Não envia o formulario
-    e.preventDefault()
 
-    if(nomeProduto.value == '' && precoProduto.value =='' && quantidadeP.value == ''){
-        console.log('erro')
-    }else{
-        const estoque=[
-            {nome:`${nomeProduto.value}`,preco:`${precoProduto.value}`,qtd:`${quantidadeP.value}`}
-        ]
-        alert(`Cadastro realizado com sucesso! Seu produto foi adicionado ao sistema.`)
 
-        console.log(estoque)
-    }
-    nomeProduto.value=''
-    precoProduto.value=''
-    quantidadeP.value=''
-
-})
