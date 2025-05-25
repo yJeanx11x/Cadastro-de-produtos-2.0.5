45d605b68b53ca6b868b9126cea98625ddd54a56
Jean Lucas <j3anlucas010@gmail.com>
2025-05-25 11:47:45 -0300

    feat: :sparkles: adiciona o a msg de cadastro de produtose remove a msg de cadastro de produtos apos 4s.

--- script/script.js
+++ script/script.js
@@ -3,39 +3,47 @@ const precoProduto = document.querySelector("#preco");
 const quantidadeP = document.querySelector("#quantidade");
 const btnCadastro = document.querySelector("#btn-cadastro");
 
+const alerta = document.querySelector(".msg-cadastro");
+
 btnCadastro.addEventListener("click", (e) => {
   //Não envia o formulario
   e.preventDefault();
-  
+
   // Armazena o Nomes dos Produtos
-  const nomesProduto = ['xbox s s'];
-  
-  // verifica se os campos estão prenchidos se não erro 
+  const nomesProduto = ["xbox s s"];
+
+  // verifica se os campos estão preenchidos se não erro
   if (
-      nomeProduto.value == "" && precoProduto.value == "" && quantidadeP.value == "") {
-          console.log("erro");
-        }
-        // Verifica se á um produto com esse nome 
-        if(nomesProduto.includes(`${nomeProduto.value}`)== true){
-            alert('⚠️ Este produto já está cadastrado no sistema.')
-            
-        }else {
-            nomesProduto.push(`${nomeProduto.value}`);
-            const produto = {
-      id:`${nomesProduto.length}`,
+    nomeProduto.value == "" &&
+    precoProduto.value == "" &&
+    quantidadeP.value == ""
+  ) {
+    console.log("erro");
+  } else {
+    nomesProduto.push(`${nomeProduto.value}`);
+    const produto = {
+      id: `${nomesProduto.length - 1}`,
       nome: `${nomeProduto.value}`,
       preco: `${precoProduto.value}`,
       qtd: `${quantidadeP.value}`,
     };
-    return produto 
+    console.log(produto);
+
+    // adiciona o a msg de cadastro de produtos.
+    alerta.classList.remove("remover");
+
+    // remove a msg de cadastro de produtos apos 4s.
+    setTimeout(() => {
+      alerta.classList.add("remover");
+    }, 4000);
+    
   }
-  
   nomeProduto.value = "";
   precoProduto.value = "";
   quantidadeP.value = "";
-  
+
+  // Verifica se á um produto com esse nome
+  if (nomesProduto.includes(`${nomeProduto.value}`) == true) {
+    alert("⚠️ Este produto já está cadastrado no sistema.");
+  }
 });
-
-
-
-
